a37e807216b7e3694e3ce126391f4bc3ed02287a
Nickolas Shtayn <nickolasshtayn@protonmail.com>
2025-08-23 20:25:11 -0500

    Adjust program structure and append to last item under task heading in noteplan

--- src/index.ts
+++ src/index.ts
@@ -1,115 +1,135 @@
 #!/usr/bin/env node
-import fsp from "fs/promises";
-import fs, { existsSync, readFileSync, writeFileSync } from "fs";
+import fs, { existsSync, mkdirSync, readFileSync, writeFileSync } from "fs";
 import path from "path";
 import { randomUUID } from "crypto";
 
 const args = process.argv.slice(2);
 
 if (!args.length) {
-  console.error("Usage: sir <card title> --d <optional description>");
+  console.error("Usage: sir <card title> -d <optional description>");
   process.exit(1);
 }
 
-let title;
-let description;
+let cardTitle: string;
+let cardDescription: string | null;
+const cardId = randomUUID().toUpperCase();
 
-if (args.includes("--d")) {
-  const descriptionIndex = args.indexOf("--d");
-  description = args.slice(descriptionIndex + 1).join(" ");
-  title = args.slice(0, descriptionIndex).join(" ");
-} else {
-  description = null;
-  title = args.join(" ");
+function cfAbsoluteTimeNow(date: Date = new Date()): number {
+  const appleEpochMs = new Date("2001-01-01T00:00:00Z").getTime();
+  return Math.floor((date.getTime() - appleEpochMs) / 1000);
 }
 
-const id = randomUUID().toUpperCase();
+if (args.includes("-d")) {
+  const descriptionIndex = args.indexOf("-d");
+  cardDescription = `# Context \n - ${args
+    .slice(descriptionIndex + 1)
+    .join(" ")}`;
+  cardTitle = args.slice(0, descriptionIndex).join(" ");
+} else {
+  cardDescription = null;
+  cardTitle = args.join(" ");
+}
 
-// const dir = path.join(
-//   path.normalize(
-//     "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs"
-//   ),
-//   "RetrievalKanban",
-//   "AllItems"
-// );
+function addCardToNotePlan() {
+  const newTask = `* Add ${cardTitle} to Ahmni`;
+  const today = new Date();
+  const year = today.getFullYear();
+  const month = "0" + (today.getMonth() + 1);
+  const day = today.getDate();
 
-// if (!fs.existsSync(dir)) {
-//   fs.mkdirSync(dir, { recursive: true });
-// }
+  const notePlanFile = `${year}${month}${day}.md`;
+  const calendarNotesPath = path.normalize(
+    "/Users/nickolas.shtayn/Library/Containers/co.noteplan.NotePlan3/Data/Library/Application Support/co.noteplan.NotePlan3/Calendar"
+  );
 
-// async function createKanbanBoard() {
-//   const kanbanBoardContents = {
-//     columns: [
-//       {
-//         itemIds: [id],
-//         title: "Column",
-//         id: randomUUID().toUpperCase(), // replace with column you want
-//         format: "full",
-//       },
-//     ],
-//   };
+  const exactPath = path.join(calendarNotesPath, notePlanFile);
+  const noteContentByLine = readFileSync(exactPath, "utf8").split("\n");
+  const endOfTaskSectionIndex = noteContentByLine.indexOf("---");
+  noteContentByLine.splice(endOfTaskSectionIndex, 0, newTask);
+  const updatedNoteContent = noteContentByLine.join("\n");
 
-//   await fsp.writeFile(
-//     path.join(
-//       path.normalize(
-//         "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs"
-//       ),
-//       "RetrievalKanban",
-//       "KanbanBoard.json"
-//     ),
-//     JSON.stringify(kanbanBoardContents)
-//   );
-// }
+  writeFileSync(exactPath, updatedNoteContent);
+}
 
-// function cfAbsoluteTimeNow(date: Date = new Date()): number {
-//   const appleEpochMs = new Date("2001-01-01T00:00:00Z").getTime();
-//   return Math.floor((date.getTime() - appleEpochMs) / 1000);
-// }
+const card = {
+  id: cardId,
+  title: cardTitle,
+  shortText: "---",
+  longText: cardDescription ? cardDescription : "",
+  date: cfAbsoluteTimeNow(),
+  colorTag: "1.0004385709762573,0.22760793566703796,0.18670153617858887,1.0",
+  fileIDs: [],
+};
 
-// async function createRetrievalCard() {
-//   const retrievalCardContents = {
-//     title,
-//     colorTag: "1.0004385709762573,0.22760793566703796,0.18670153617858887,1.0",
-//     shortText: "sir-cli",
-//     longText: description,
-//     id,
-//     fileIDs: [],
-//     date: cfAbsoluteTimeNow(),
-//   };
+const cardPath = path.join(
+  "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs",
+  "RetrievalKanban",
+  "AllItems",
+  `${cardId}.KanbanItem.json`
+);
 
-//   if (fs.existsSync(path.join(path.normalize(), "RetrievalKanban", "AllItems")))
-//     await fsp.writeFile(
-//       path.join(
-//         path.normalize(
-//           "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs"
-//         ),
-//         "RetrievalKanban",
-//         `/AllItems/${id}.KanbanItem.json`
-//       ),
-//       JSON.stringify(retrievalCardContents),
-//       {
-//         flag: "wx",
-//       }
-//     );
-// }
+if (
+  existsSync(
+    path.join(
+      "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs",
+      "RetrievalKanban"
+    )
+  )
+) {
+  const kanbanColumn = JSON.parse(
+    readFileSync(
+      path.join(
+        "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs",
+        "RetrievalKanban",
+        "KanbanBoard.json"
+      ),
+      "utf8"
+    )
+  );
+  const currentCardIds = kanbanColumn.columns[0].itemIds;
+  currentCardIds.push(cardId);
 
-// function addNotePlanTask() {
-//   const newTask = `\n* Add ${title} to Ahmni`;
-//   const today = new Date();
-//   const year = today.getFullYear();
-//   const month = "0" + (today.getMonth() + 1);
-//   const day = today.getDate();
+  writeFileSync(
+    path.join(
+      "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs",
+      "RetrievalKanban",
+      "KanbanBoard.json"
+    ),
+    JSON.stringify(kanbanColumn)
+  );
 
-//   const notePlanFile = `${year}${month}${day}.md`;
-//   const calendarNotesPath = path.normalize(
-//     "/Users/nickolas.shtayn/Library/Containers/co.noteplan.NotePlan3/Data/Library/Application Support/co.noteplan.NotePlan3/Calendar"
-//   );
+  writeFileSync(cardPath, JSON.stringify(card));
+  addCardToNotePlan();
+} else {
+  mkdirSync(
+    path.join(
+      "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs",
+      "RetrievalKanban",
+      "AllItems"
+    ),
+    { recursive: true }
+  );
 
-//   const exactPath = path.join(calendarNotesPath, notePlanFile);
-//   const noteContent = readFileSync(exactPath, "utf8");
-//   writeFileSync(exactPath, noteContent + newTask);
-// }
+  const kanbanBoardContents = {
+    columns: [
+      {
+        itemIds: [cardId],
+        id: "B40E8971-9B3A-4B37-950F-5E336458512A",
+        format: "full",
+        title: "Prior Knowledge",
+      },
+    ],
+  };
 
-// createKanbanBoard();
-// createRetrievalCard();
-// addNotePlanTask();
+  writeFileSync(
+    path.join(
+      "/Users/nickolas.shtayn/Library/Mobile Documents/com~apple~CloudDocs",
+      "RetrievalKanban",
+      "KanbanBoard.json"
+    ),
+    JSON.stringify(kanbanBoardContents)
+  );
+
+  writeFileSync(cardPath, JSON.stringify(card));
+  addCardToNotePlan();
+}
